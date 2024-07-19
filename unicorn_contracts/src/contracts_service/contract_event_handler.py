fa696bb4520e07ab256eeadc1bff9b22c0290290
Sandesh Lama <sandesh@ip-10-10-1-139.ec2.internal>
2024-07-19 13:44:34 +0545

    Completed upto creating CodePipeline

--- unicorn_contracts/src/contracts_service/contract_event_handler.py
+++ unicorn_contracts/src/contracts_service/contract_event_handler.py
@@ -27,7 +27,7 @@ def lambda_handler(event: dict, context: dict):
     for record in event['Records']:
         http_method = record['messageAttributes'].get('HttpMethod', {}).get('stringValue')
         body = json.loads(record['body'])
-        
+
         if http_method == 'POST':
             create_contract(body)
         elif http_method == 'PUT':
@@ -36,58 +36,74 @@ def lambda_handler(event: dict, context: dict):
             raise Exception(f'Unable to handle HttpMethod {http_method}')
 
 
-def create_contract(event: dict) -> None:
-    """Create contract inside DynamoDB table
-
-    Execution logic:
-        if contract does not exist
-        or contract status is either of [ CANCELLED | CLOSED | EXPIRED]
-        then
-            create or replace contract with status = DRAFT
-            log response
-            log trace info
-            return
-        else
-            log exception message
-
-    Parameters
-    ----------
-        contract (dict): _description_
-
-    Returns
-    -------
-    dict
-        DynamoDB put Item response
-    """
-
-    # TODO: create entry in DDB for new contract
-    raise NotImplementedError
-
-
 def update_contract(contract: dict) -> None:
-    """Update an existing contract inside DynamoDB table
 
-    Execution logic:
+    print(f"Updating contract: {contract}")
 
-        if  contract exists exist
-        and contract status is either of [ DRAFT ]
-        then
-            update contract status to APPROVED
-            log response
-            log trace info
-            return
-        else
-            log exception message
+    try:
+        contract["contract_status"] = ContractStatus.APPROVED.name
+        current_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-    Parameters
-    ----------
-        contract (dict): _description_
+        response = table.update_item(
+            Key={
+                'property_id': contract['property_id'],
+            },
+            UpdateExpression="set contract_status=:t, modified_date=:m",
+            ConditionExpression=
+                Attr('property_id').exists()
+              & Attr('contract_status').is_in([
+                  ContractStatus.DRAFT.name
+                ]),
+            ExpressionAttributeValues={
+                ':t': contract['contract_status'],
+                ':m': current_date,
+            },
+            ReturnValues="UPDATED_NEW")
+        print(f'var:response - "{response}"')
 
-    Returns
-    -------
-    dict
-        DynamoDB put Item response
-    """
+    except ClientError as e:
+        code = e.response["Error"]["Code"]
+        if code == 'ConditionalCheckFailedException':
+            print(f"Unable to update contract Id {contract['property_id']}. Status is not in status DRAFT")
+        elif code == 'ResourceNotFoundException':
+            print(f"Unable to update contract Id {contract['property_id']}. Not Found")
+        else:
+            raise e
 
-    # TODO: add code to update the contract in DynamoDB table
-    raise NotImplementedError
+
+def create_contract(event: dict) -> None:
+
+    current_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    contract = {
+        "property_id":                  event["property_id"],  # PK
+        "address":                      event["address"],
+        "seller_name":                  event["seller_name"],
+        "contract_created":             current_date,
+        "contract_last_modified_on":    current_date,
+        "contract_id":                  str(uuid.uuid4()),
+        "contract_status":              ContractStatus.DRAFT.name,
+    }
+
+    print(f"Creating contract: {contract} From event: {event}")
+
+    try:
+        response = table.put_item(
+            Item=contract,
+            ConditionExpression=
+                Attr('property_id').not_exists()
+              | Attr('contract_status').is_in([
+                  ContractStatus.CANCELLED.name,
+                  ContractStatus.CLOSED.name,
+                  ContractStatus.EXPIRED.name,
+                ]))
+        print(f'var:response - "{response}"')
+
+    except ClientError as e:
+        code = e.response["Error"]["Code"]
+        if code == 'ConditionalCheckFailedException':
+            print(f"""
+                    Unable to create contract for Property {contract['property_id']}.
+                    There already is a contract for this property in status {ContractStatus.DRAFT.name} or {ContractStatus.APPROVED.name}
+                    """)
+        else:
+            raise e
