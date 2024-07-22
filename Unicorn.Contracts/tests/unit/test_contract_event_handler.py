c2ed9bb1fd27d240c6bef490534e3f3c3da3a0eb
Sandesh Lama <sandesh@ip-10-10-1-139.ec2.internal>
2024-07-22 12:47:36 +0545

    Adding aws-lambda-powertools as dependency in pyproject.toml file

--- Unicorn.Contracts/tests/unit/test_contract_event_handler.py
+++ Unicorn.Contracts/tests/unit/test_contract_event_handler.py
@@ -17,10 +17,10 @@ from .helper import create_ddb_table_contracts, create_test_sqs_ingestion_queue,
 def test_valid_create_event(dynamodb, sqs, lambda_context):
     payload = load_event('create_contract_valid_1')
     event = sqs_event([{'body': payload, 'attributes': {'HttpMethod': 'POST'}}])
-    
+
     # Loading function here so that mocking works correctly.
     from contracts_service import contract_event_handler  # noqa: F401
-    # Reload is required to prevent function setup reuse from another test 
+    # Reload is required to prevent function setup reuse from another test
     reload(contract_event_handler)
 
     create_ddb_table_contracts(dynamodb)
@@ -47,7 +47,7 @@ def test_valid_update_event(dynamodb, sqs, lambda_context):
 
     # Loading function here so that mocking works correctly.
     from contracts_service import contract_event_handler  # noqa: F401
-    # Reload is required to prevent function setup reuse from another test 
+    # Reload is required to prevent function setup reuse from another test
     reload(contract_event_handler)
 
     create_ddb_table_contracts_with_entry(dynamodb)
@@ -84,7 +84,7 @@ def test_wrong_dynamodb_table(dynamodb, lambda_context):
     event = sqs_event([{'body': load_event('create_contract_valid_1'), 'attributes': {'HttpMethod': 'POST'}}])
 
     from contracts_service import contract_event_handler  # noqa: F401
-    
+
     create_ddb_table_contracts(dynamodb)
 
     with pytest.raises(ClientError):
